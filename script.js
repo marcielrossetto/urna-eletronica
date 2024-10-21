85498f556ef4f3baa359a881578571875a8784ad
marcielrossetto <40443010+marcielrossetto@users.noreply.github.com>
2024-10-21 19:55:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const votos = {
+    BOLSONARO: 0,
+    TARCISIO: 0,
+    LULA: 0,
+};
+
+const cliques = {
+    BOLSONARO: 0,
+    TARCISIO: 0,
+    LULA: 0,
+};
+
+let percentualTransferidoBolsonaro = 0;
+let percentualTransferidoTarcisio = 0;
+
+function votar(candidato) {
+    cliques[candidato]++;
+    if (candidato === "BOLSONARO") {
+        percentualTransferidoBolsonaro += 15;
+        votos[candidato]++;
+        if (percentualTransferidoBolsonaro >= 100) {
+            votos["LULA"]++;
+            votos[candidato]--; // Remove um voto de Bolsonaro
+            percentualTransferidoBolsonaro -= 100;
+        }
+    } else if (candidato === "TARCISIO") {
+        percentualTransferidoTarcisio += 10;
+        votos[candidato]++;
+        if (percentualTransferidoTarcisio >= 100) {
+            votos["LULA"]++;
+            votos[candidato]--; // Remove um voto de Tarcisio
+            percentualTransferidoTarcisio -= 100;
+        }
+    } else if (candidato === "LULA") {
+        votos[candidato]++;
+    }
+    exibirResultado();
+}
+
+function exibirResultado() {
+    let resultado = "";
+    for (const candidato in votos) {
+        resultado += `${candidato}: ${votos[candidato]} votos (${cliques[candidato]} cliques)<br>`;
+    }
+    document.getElementById("resultado").innerHTML = resultado;
+}
+
+function mostrarTotal() {
+    let totalCliques = 0;
+    let totalVotos = 0;
+    for (const candidato in votos) {
+        totalCliques += cliques[candidato];
+        totalVotos += votos[candidato];
+    }
+    document.getElementById("resultadoTotal").innerHTML = 
+        `Total de cliques: ${totalCliques}<br>Total de votos válidos: ${totalVotos}`;
+}
